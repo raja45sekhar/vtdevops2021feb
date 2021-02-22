e8bb4fc8192601e90df19881c262e502563759ac
Pavan Bhavani Shekhar Devarakonda <bhavanishekhar@gmail.com>
2021-02-22 05:33:19 +0530

    Update gitinstall.sh
    Updated all hard-coded version numbers to variable VER.

--- scripts/gitinstall.sh
+++ scripts/gitinstall.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # Install Git latest version on RHEL flavours
 # Download this script or use git clone then run regular shell script execution
+# ./gitinstall.sh
 # Created: 20-Feb-2021
 # 
 
+VER=2.30.1
 # prerequisites
 echo "Install dependencies"
 yum install -y wget \
 curl-devel expat-devel gettext-devel openssl-devel zlib-devel \
 gcc perl-ExtUtils-MakeMaker
 cd /opt
-wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-2.30.1.tar.gz
+wget https://mirrors.edge.kernel.org/pub/software/scm/git/git-${VER}.tar.gz
 
 echo "Check git installed version after dependencies installed..."
 git --version
@@ -23,8 +25,8 @@ fi
 
 # extract the git tarball
 echo "Extracting git installer"
-tar -zxf git-2.30.1.tar.gz
-cd git-2.30.1
+tar -zxf git-${VER}.tar.gz
+cd git-${VER}
 
 # installing latest version of git 
 echo "Compile and install git latest version"
@@ -36,9 +38,9 @@ echo "Adding to PATH"
 echo "export PATH=/usr/local/git/bin:$PATH" >> /etc/bashrc
 source /etc/bashrc
  
-git --version | grep 2.30
+git --version | grep ${VER}
 [ $? -eq 0 ] && echo "Git installed latest version successfully"
 
 # clean up
 echo "Clean up the git installer stuff"
-cd /opt; rm -rf git-2.30.1 git-2.30.1.tar.gz
+cd /opt; rm -rf git-${VER} git-${VER}.tar.gz
