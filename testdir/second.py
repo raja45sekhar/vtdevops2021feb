630dbad3a4de00e14be2c2ea062f873294a0c0f5
vagrant <vagrant@vagrant.vm>
2021-02-27 04:09:11 +0000

    adding testdir

---
+++ testdir/second.py
@@ -0,0 +1,15 @@
+def sum(x,y);
+    return x+y
+# main program starts here
+print("calling the function in output")
+print(sum(10,20))
+print("call the function assigning to varaible")
+a=sum(300,500)
+print(a)
+print("calling function in expression")
+x=20 
+y=15
+z=sum(x,y)*2
+print(z)
+
+
