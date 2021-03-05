fae28cf4c57dfb04acd9bcecd53c7168e22cd720
srini12vas <75303276+srini12vas@users.noreply.github.com>
2021-03-05 20:46:35 +0530

    Update third.py

--- testdir/third.py
+++ testdir/third.py
@@ -1,5 +1,6 @@
 #this program list 
-Devops=["Raja","srinivas","vignesh","srikanth","melvin"]
+Devops=["Raja","srinivas","vignesh","srikanth","melvin"] #Devops of Participents
+
 for p in Devops:
     print("Devops learner:",p)
 
