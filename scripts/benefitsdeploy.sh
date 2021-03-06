bd1c2cb9c8379a50edd5fd5891da5cb0204db26b
Srikanth Rao <sirikanthrao@gmail.com>
2021-03-06 04:05:17 +0000

    added a comment to benefits file

--- scripts/benefitsdeploy.sh
+++ scripts/benefitsdeploy.sh
@@ -1,3 +1,4 @@
+# This script is to deploy benefits app on admin server
 java weblogic.Deployer -adminurl t3://192.168.33.110:8001 \
      -username weblogic -password Welcome1 \
      -deploy /vagrant/data/benefits.war -id benefits
